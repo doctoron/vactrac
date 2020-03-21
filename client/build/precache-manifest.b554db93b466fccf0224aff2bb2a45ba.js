2ad51c7e04c2d87adb5d75d9ed7635aafb358b2d
doctoron <ron.antonio@me.com>
2020-03-20 22:52:23 -0400

    updated links

---
+++ client/build/precache-manifest.b554db93b466fccf0224aff2bb2a45ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5187713a7872f3fa35f7346d70110b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b770fb73c2f7a0043ed7",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "5813edd4ede622bc6c44",
+    "url": "/static/css/main.716b55f4.chunk.css"
+  },
+  {
+    "revision": "b770fb73c2f7a0043ed7",
+    "url": "/static/js/2.cbeb9595.chunk.js"
+  },
+  {
+    "revision": "d190cbdfa52f586dbdfc7733ef49f30a",
+    "url": "/static/js/2.cbeb9595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5813edd4ede622bc6c44",
+    "url": "/static/js/main.6b408e18.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "5e547cf0b19600521e16be3678030cf1",
+    "url": "/static/media/adult-celebrate2.5e547cf0.jpg"
+  },
+  {
+    "revision": "9378bb5d56c43d6998a16c43f8b76437",
+    "url": "/static/media/puzzle12.9378bb5d.jpg"
+  },
+  {
+    "revision": "d70dc6f406cc5f1b34cec655a7e155a0",
+    "url": "/static/media/roundAbout2-Logo.d70dc6f4.jpg"
+  }
+]);
